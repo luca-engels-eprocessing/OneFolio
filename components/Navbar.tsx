38a98f29e1c553d0876d5f28492c1427c61b5b66
Luca-Engels <luca.engels@student.fontys.nl>
2024-04-09 14:27:50 +0200

    padding for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
 
 
     return (
-        <div className={"sticky top-0 flex justify-center items-center h-screen"}>
+        <div className={"sticky top-0 flex justify-center items-center h-screen p-8"}>
             <div
                 className="border-def transition-colors bg-secLight dark:bg-secDark flex flex-col justify-center gap-2 items-center p-4 rounded-3xl">
                 <Image src={"/OneFolio_2.png"} alt={"One Folio"} width={150} height={150} className={"absolute top-8"}/>
