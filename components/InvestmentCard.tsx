5e62a4134e651c314158ce1e558602c7d690e118
Luca-Engels <luca.engels@student.fontys.nl>
2024-04-09 15:58:05 +0200

    added expansion to Cards

--- components/InvestmentCard.tsx
+++ components/InvestmentCard.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
+
 
 type Props = {
     title: string;
@@ -13,9 +15,29 @@ type Props = {
 }
 
 const InvestmentCard = (props: Props) => {
+const expandRef = useRef<HTMLDivElement>(null);
+const gridRef = useRef<HTMLDivElement>(null);
+
+//TODO expand button height to h-96 if button is pressed
+
+const onClickExpand = () => {
+    if(expandRef.current){
+        expandRef.current.classList.toggle('h-96')
+    }
+    if(gridRef.current){
+        if(gridRef.current.classList.toggle("grid-rows-5")){
+            gridRef.current.classList.remove("grid-rows-2");
+        }
+        else{
+            gridRef.current.classList.add("grid-rows-2");
+        }
+        
+    }
+}
+
   return (
-    <div className={"btn-nav rounded-md flex flex-row gap-8 group parent"}>
-        <div className={"flex flex-col justify-between w-1/2 m-8 "+props.className}>
+    <button className={"btn-nav rounded-md flex flex-row gap-8 group parent"} onClick={onClickExpand}>
+        <div className={"flex flex-col justify-between w-1/2 m-8 "+props.className} ref={expandRef}>
             <h1 className={"text-accentLight dark:text-accentDark text-5xl font-semibold pb-8 group-hover:text-accentTextLight dark:group-hover:text-accentTextDark"}>{props.title}</h1>
             <div>
                 <p className={"text-sm font-light"}>Läuft aus am:</p>
@@ -25,7 +47,7 @@ const InvestmentCard = (props: Props) => {
         <div
           className={"border-r-2 border-accentLight dark:border-accentDark my-4"}
         />
-        <div className={"grid grid-row2 grid-cols-2 w-1/2 m-8 items-center"}>
+        <div className={"grid grid-row2 gap-y-4 grid-cols-2 w-1/2 m-8 items-center"} ref={gridRef}>
             <div className='row-start-1 col-start-1'>
                 <p className={"text-sm font-light"}>{props.key1}</p>
                 <p className={"text-2xl font-normal"}>{props.value1}</p>
@@ -40,8 +62,7 @@ const InvestmentCard = (props: Props) => {
             </div>
             <p className='row-start-2 col-start-2'>Anpassen ...</p>
         </div>
-
-    </div>
+    </button>
   )
 }
 
