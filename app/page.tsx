7c47675b84527b57a17436be5223131a8d30600b
Luca-Engels <luca.engels@student.fontys.nl>
2024-04-11 17:09:00 +0200

    Graph BG

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use server"
+
 async function Home() {
   return (
       <main className="flex h-full w-full flex-col gap-16 py-24 xl:pl-48 px-16">
@@ -10,29 +11,25 @@ async function Home() {
               <div className={"row-start-1 col-start-1 flex flex-col p-4 content-center justify-center text-center"}>
                   <p className={"text-2xl font-medium"}>Ihre Branchen</p>
                   <div
-                      className={"border-def w-full h-48 bg-red-700 hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
-                      <p>GRAPH 1 HERE</p>
+                      className={"border-def w-full h-48 bg-[url('/graphs/meta-chart.png')] bg-contain bg-top bg-no-repeat hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
                   </div>
               </div>
               <div className={"row-start-1 col-start-2 flex flex-col p-4 content-center justify-center text-center"}>
                   <p className={"text-2xl font-medium"}>Ihre Sparte</p>
                   <div
-                      className={"border-def w-full h-48 bg-green-700 hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
-                      <p>GRAPH 1 HERE</p>
+                      className={"border-def w-full h-48 bg-[url('/graphs/meta-chart(1).png')] bg-contain bg-top bg-no-repeat hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
                   </div>
               </div>
               <div className={"row-start-2 col-start-1 flex flex-col p-4 content-center justify-center text-center"}>
                   <p className={"text-2xl font-medium"}>Laufzeitbindungen</p>
                   <div
-                      className={"border-def w-full h-48 bg-blue-700 hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
-                      <p>GRAPH 1 HERE</p>
+                      className={"border-def w-full h-48 bg-[url('/graphs/meta-chart(3).png')] bg-contain bg-top bg-no-repeat hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
                   </div>
               </div>
               <div className={"row-start-2 col-start-2 flex flex-col p-4 content-center justify-center text-center"}>
                   <p className={"text-2xl font-medium"}>Ihre Risikoklassen</p>
                   <div
-                      className={"border-def w-full h-48 bg-yellow-700 hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
-                      <p>GRAPH 1 HERE</p>
+                      className={"border-def w-full h-48 bg-[url('/graphs/meta-chart(4).png')] bg-contain bg-top bg-no-repeat hover:shadow-[5px_5px_10px_5px_rgba(0,0,0,0.2)] dark:shadow"}>
                   </div>
               </div>
           </div>
