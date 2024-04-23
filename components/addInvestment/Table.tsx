735a2763699d58bf7048e0d7fa976da6d83de596
Luca Engels <70887060+Luca-Engels@users.noreply.github.com>
2024-04-23 12:59:01 +0200

    AddingChanges

--- components/addInvestment/Table.tsx
+++ components/addInvestment/Table.tsx
@@ -34,40 +34,7 @@ const Table = (props: Props) => {
     
     const createValueButton = (key: string, value: any, index: number, forKey: string) => (
         <button className='btn-nav rounded-md flex flex-row justify-center gap-8 px-4 group p-2 w-full' key={index}
-            onClick={() => {
-                setDisplayed(false)
-                                if("Mehr..." == forKey){
-                    delete modifyableList[forKey as keyof typeof modifyableList][key];
-                    var newModList = {}
-                    Object.entries(modifyableList).map(([keyI, valueI]) => {
-                        if (keyI === forKey) {
-                            newModList = {...newModList, ...value}
-                        }
-                        newModList = {...newModList, [keyI]: valueI};
-                    })
-                    setModifyableList(newModList)
-                }
-                else if(typeof value === 'string') {
-                    setselectionList({ ...selectionList, [forKey]: value});
-                }
-                else{
-                    var newModList = {}
-                    Object.entries(modifyableList).map(([keyI, valueI]) => {
-                        
-
-                        if (keyI in value) {
-                            delete selectionList[keyI as keyof typeof selectionList];
-                            return;
-                        }
-                        newModList = {...newModList, [keyI]: valueI};
-                        if (keyI === forKey) {
-                            newModList = {...newModList, ...value};
-                        }
-                    })
-                    setModifyableList(newModList);
-                    setselectionList({ ...selectionList, [forKey]: key });
-                }
-            }}>
+            onClick={valueButtonOnClick()}>
             <p className='w-full px-8 text-left 2xl:text-2xl xl:text-lg lg:text-2xl py-2'>{typeof value === 'string' ? value : key}</p>
         </button>
     );
@@ -88,54 +55,7 @@ const Table = (props: Props) => {
     // TODO Sub of Sub category not working (Also do not implement)
     const createAddButton = (node:string) => {
         return (
-            <form className='btn-nav rounded-md flex flex-col justify-center gap-8 px-4 group p-2 w-full' onSubmit={(e) => {
-                e.preventDefault();
-                const newCat = (e.target as HTMLFormElement)['newCategory'].value;
-                const subCat = (e.target as HTMLFormElement)['subCategory'].value;
-                if(!subCat){
-                    const curr = modifyableList[node as keyof typeof modifyableList]
-                    var valueOfKey = ""
-                    var keyOfKey = ""
-                    //iterate and check if curr is in modyfiableList
-                    Object.entries(modifyableList).map(([key, value]) => {
-                        if(!Array.isArray(value)&&value != null &&typeof value === 'object'){
-                            Object.entries(value).map(([keyI, valueI]) => {
-                                if(valueI[node]&&valueI[node]==curr){
-                                    valueOfKey = keyI
-                                    keyOfKey = key
-                                }
-
-                            })
-                        }
-                    })
-                    console.log(curr)
-                    console.log(findListInObject(curr,modifyableList))
-                    if(valueOfKey){
-                        const list = [...modifyableList[keyOfKey as keyof typeof modifyableList][valueOfKey][node], newCat]
-                        const listKey = {...modifyableList[keyOfKey as keyof typeof modifyableList] as {}, [node]: list}
-                        const valueKey = {...modifyableList[keyOfKey as keyof typeof modifyableList] as {},[valueOfKey]: listKey}
-                        console.log({[keyOfKey]:valueKey})
-                        setModifyableList({...modifyableList, [node]: list, [keyOfKey]:valueKey})
-                    }
-                    else if(Array.isArray(curr)){
-                        const list = [...curr, newCat]
-                        setModifyableList({...modifyableList, [node]: list})
-                    }
-                    else{
-                        const l = Object.keys(curr).length
-                        setModifyableList({...modifyableList, [node]: {...modifyableList[node as keyof typeof modifyableList] as {},...{[l]:newCat}}})
-                    }
-                }
-                else{
-                    const newCate = {[newCat]: {[subCat]:[]}}
-                    const curr = modifyableList[node as keyof typeof modifyableList] as {}
-                    const item = {[node]: {...curr, ...newCate}}
-                    setModifyableList({...modifyableList, ...item})
-                }
-                setDisplayed(false)
-                setselectionList({...selectionList, [node]: newCat})
-
-            }}>
+            <form className='btn-nav rounded-md flex flex-col justify-center gap-8 px-4 group p-2 w-full' onSubmit={(e) => {handleFormSubmit(e,node)}}>
                 <input type='text' className='w-full px-8 text-left text-3xl py-2 border-0 bg-transparent' name='newCategory' placeholder='Hinzufügen ...' />
                 <input type='text' className='w-full px-8 text-left text-3xl py-2 border-0 bg-transparent' name='subCategory' placeholder='Untergruppe? ...' />
                 <div className='flex flex-row text-3xl gap-8 items-center '>
@@ -145,6 +65,74 @@ const Table = (props: Props) => {
                 </div>
             </form>
         );
+    }
+    
+
+    const valueButtonOnClick = () => {
+        setDisplayed(false)
+                        if("Mehr..." == forKey){
+            delete modifyableList[forKey as keyof typeof modifyableList][key];
+            var newModList = {}
+            Object.entries(modifyableList).map(([keyI, valueI]) => {
+                if (keyI === forKey) {
+                    newModList = {...newModList, ...value}
+                }
+                newModList = {...newModList, [keyI]: valueI};
+            })
+            setModifyableList(newModList)
+        }
+        else if(typeof value === 'string') {
+            setselectionList({ ...selectionList, [forKey]: value});
+        }
+        else{
+            var newModList = {}
+            Object.entries(modifyableList).map(([keyI, valueI]) => {
+                
+
+                if (keyI in value) {
+                    delete selectionList[keyI as keyof typeof selectionList];
+                    return;
+                }
+                newModList = {...newModList, [keyI]: valueI};
+                if (keyI === forKey) {
+                    newModList = {...newModList, ...value};
+                }
+            })
+            setModifyableList(newModList);
+            setselectionList({ ...selectionList, [forKey]: key });
+        }
+    }
+
+    const handleFormSubmit = (e:any,node:string) => {
+        e.preventDefault();
+        const newCat = (e.target as HTMLFormElement)['newCategory'].value;
+        const subCat = (e.target as HTMLFormElement)['subCategory'].value;
+        if(!subCat){
+            const curr = modifyableList[node as keyof typeof modifyableList]
+            //iterate and check if curr is in modyfiableList
+            const listKeyValue = findListInObject(curr,modifyableList)
+            console.log(curr)
+            console.log(listKeyValue)
+            if(listKeyValue){
+                setModifyableList({...modifyableList, ...listKeyValue})
+            }
+            else if(Array.isArray(curr)){
+                const list = [...curr, newCat]
+                setModifyableList({...modifyableList, [node]: list})
+            }
+            else{
+                const l = Object.keys(curr).length
+                setModifyableList({...modifyableList, [node]: {...modifyableList[node as keyof typeof modifyableList] as {},...{[l]:newCat}}})
+            }
+        }
+        else{
+            const newCate = {[newCat]: {[subCat]:[]}}
+            const curr = modifyableList[node as keyof typeof modifyableList] as {}
+            const item = {[node]: {...curr, ...newCate}}
+            setModifyableList({...modifyableList, ...item})
+        }
+        setDisplayed(false)
+        setselectionList({...selectionList, [node]: newCat})
     }
 
     const createValueList = (node: string) => {
---
+++ components/addInvestment/ValueListHandler.tsx
@@ -0,0 +1,50 @@
+const createList = (values:{}, listType:any,) => {
+    switch(typeof listType){
+        case 'number':
+            break
+        case 'date':
+            break
+        case 'boolean':
+            break
+        case 'string':
+            break
+        case 'object':
+            break
+    }
+}
+
+
+const valueButtonOnClick = () => {
+    setDisplayed(false)
+                    if("Mehr..." == forKey){
+        delete modifyableList[forKey as keyof typeof modifyableList][key];
+        var newModList = {}
+        Object.entries(modifyableList).map(([keyI, valueI]) => {
+            if (keyI === forKey) {
+                newModList = {...newModList, ...value}
+            }
+            newModList = {...newModList, [keyI]: valueI};
+        })
+        setModifyableList(newModList)
+    }
+    else if(typeof value === 'string') {
+        setselectionList({ ...selectionList, [forKey]: value});
+    }
+    else{
+        var newModList = {}
+        Object.entries(modifyableList).map(([keyI, valueI]) => {
+            
+
+            if (keyI in value) {
+                delete selectionList[keyI as keyof typeof selectionList];
+                return;
+            }
+            newModList = {...newModList, [keyI]: valueI};
+            if (keyI === forKey) {
+                newModList = {...newModList, ...value};
+            }
+        })
+        setModifyableList(newModList);
+        setselectionList({ ...selectionList, [forKey]: key });
+    }
+}
