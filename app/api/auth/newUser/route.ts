fef2b0fcaee55c422d640543eb4a6106a702a099
lud4x <luca.engels@student.fontys.nl>
2024-05-03 18:10:57 +0200

    rework to use Prisma, tried using route endpoints

--- app/api/auth/newUser/route.ts
+++ app/api/auth/newUser/route.ts
@@ -1,41 +1,37 @@
 
 import {NextRequest, NextResponse } from "next/server";
-import {connect,disconnect, findAccount} from '@/utils/db.js'
 import bcrypt from 'bcryptjs';
-import Account from '@/models/User.js'
-
-
+import {db} from "@/utils/db"
+import {PrismaClient, Prisma } from "@prisma/client";
 
 export async function POST(req:NextRequest) {
   const jsonData= await req.json();
-  await connect();
 
-  if(await findAccount(jsonData.email)){
+  const user = await db.user.findFirst({where:{email:jsonData.email.toLowerCase() as string}})
+  if(user){
     return NextResponse.json({ message: 'User exists!'},{ status: 200 });
   }
 
   const hashPWD = await bcrypt.hash(jsonData.password, 10);
-
-  const newUser = new Account( {
-    name:{
-      firstname: jsonData.firstname,
-      lastname: jsonData.lastname,
-    },
-    password: hashPWD,
-    email: jsonData.email.toLowerCase(),
-    street: jsonData.street,
-    city: jsonData.city,
-    country: jsonData.country,
-    phone: jsonData.phone
-  })
-
+  
   try {
-    await newUser.save();
+    const newUser = await db.user.create({
+      data:{
+        name:{
+          firstname: jsonData.firstname,
+          lastname: jsonData.lastname,
+        },
+        password: hashPWD,
+        email: jsonData.email.toLowerCase(),
+        street: jsonData.street,
+        city: jsonData.city,
+        country: jsonData.country,
+        phone: jsonData.phone
+      }
+    })
     return NextResponse.json({ message: 'Data saved successfully!' },{ status: 201 });
   } catch (error) {
     return NextResponse.json({ message: 'Something went wrong!'+error },{ status: 500 });
-  } finally {
-    disconnect();
   }
 }
 
