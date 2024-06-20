97f9450f8c5fffc11f7522f8e6d8bd9d9c81fd46
luca-engels <luca.engels@student.fontys.nl>
2024-06-20 10:24:26 +0200

    fixed select

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,42 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {IconHome,IconSquarePlus,IconClipboardList,IconUserCircle} from "@tabler/icons-react";
 import { LoginButton } from "@/components/auth/LoginButton";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
 
+    const [currNav, setCurrNav] = useState("home")
+
+    var isDarkTheme=false
+    if(typeof window != "undefined"&&window&&window.matchMedia){
+        isDarkTheme=window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+
+
+  
 
     return (
         <div className={"sticky top-0 flex flex-row xl:flex-col justify-center items-center xl:h-screen h-[20vh] sm:p-8 bg-sec xl:bg-transparent"}>
-            <Image priority src={"/onefoliogo.png"} alt={"One Folio"} width={0} height={0} sizes="50vh" className={"xl:absolute h-auto w-3/4 xl:top-8 hidden sm:flex"}/>
+            {isDarkTheme?
+            <Image priority src={"/OneFoliogo.png"} alt={"One Folio"} width={0} height={0} sizes="50vh" className={"xl:absolute h-auto w-3/4 xl:top-8 hidden sm:flex"}/>:
+            <Image priority src={"/OneFoliogo_big.png"} alt={"One Folio"} width={0} height={0} sizes="50vh" className={"xl:absolute h-auto w-3/4 xl:top-8 hidden sm:flex"}/>
+            }
+            
             <div
                 className="border-def transition-colors bg-secLight dark:bg-secDark flex xl:flex-col justify-center gap-2 items-center p-4 rounded-3xl">
-                <Link href={"/"} className={"btn-nav text-center md:p-4 p-1 rounded-3xl w-full"}>
+                <Link href={"/"} onClick={()=>setCurrNav("home")} className={cn("btn-nav text-center md:p-4 p-1 rounded-3xl w-full",currNav=="home"&&"text-accentLight")}>
                     <IconHome size={32} />
                 </Link>
-                <Link href={"/add"} className={"btn-nav text-center md:p-4 p-1  rounded-3xl w-full"}>
+                <Link href={"/add"} onClick={()=>setCurrNav("add")} className={cn("btn-nav text-center md:p-4 p-1 rounded-3xl w-full",currNav=="add"&&"text-accentLight")}>
                     <IconSquarePlus size={32} />
                 </Link>
-                <Link href={"/overview"} className={"btn-nav text-center md:p-4 p-1  rounded-3xl w-full"}>
+                <Link href={"/overview"} onClick={()=>setCurrNav("overview")} className={cn("btn-nav text-center md:p-4 p-1 rounded-3xl w-full",currNav=="overview"&&"text-accentLight")}>
                     <IconClipboardList size={32} />
                 </Link>
-                <Link href={"/settings"} className={"btn-nav text-center md:p-4 p-1  rounded-3xl w-full"}>
+                <Link href={"/settings"} onClick={()=>setCurrNav("settings")} className={cn("btn-nav text-center md:p-4 p-1 rounded-3xl w-full",currNav=="settings"&&"text-accentLight")}>
                     <IconUserCircle size={32} />
                 </Link>
                 <LoginButton className={" btn-nav text-center md:p-4 p-1 rounded-3xl w-full flex xl:hidden"}/>
