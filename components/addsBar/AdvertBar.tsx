3b8d41ac8c4fecf64559fff2408e2e1a8f7104dc
lud4x <luca.engels@student.fontys.nl>
2024-06-28 10:15:24 +0200

    change layout

--- components/addsBar/AdvertBar.tsx
+++ components/addsBar/AdvertBar.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import * as C from "@/components/ui/carousel"
 import { Card,CardContent } from '../ui/card'
-import Link from 'next/link'
+import AdvertCard from './AdvertCard'
 
 
 type Props = {}
@@ -10,19 +10,11 @@ type Props = {}
 export const AdvertBar = (props: Props) => {
   return (
   <div className="p-4 xl:w-[15vw] w-auto border-def rounded-xl xl:h-fit h-1/5 bg-sec flex flex-col justify-between xl:mr-8 xl:mb-8 xl:mt-32">
-    <C.Carousel className="flex flex-col justify-between w-full h-full" orientation='vertical' opts={{loop:true}} delay={5000}>
+    <C.Carousel className="flex flex-col justify-between w-full h-full xl:grid" orientation='vertical' opts={{loop:true}} delay={5000}>
       <C.CarouselContent className='h-full'>
         {Array.from({ length: 5 }).map((_, index) => (
           <C.CarouselItem key={index}>
-            <div className="p-1 w-full h-full">
-              <Link href={"https://www.google.com"}>
-                <Card className='h-full'>
-                  <CardContent className="flex items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">Werbung: {index + 1}</span>
-                  </CardContent>
-                </Card>
-              </Link>
-            </div>
+            <AdvertCard name={"Werbung "+index + 1} imageUrl={''} index={0} />
           </C.CarouselItem>
         ))}
       </C.CarouselContent>
--- components/addsBar/AdvertCard.tsx
+++ components/addsBar/AdvertCard.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+import { Card, CardContent } from '@/components/ui/card'
 
-type Props = {}
+type Props = {
+  name:string
+  imageUrl:string
+  index:number
+}
 
 const AdvertCard = (props: Props) => {
   return (
-    <div>AdvertCard</div>
+    <div className="p-1 w-full h-full">
+      <Link href={"https://www.google.com"}>
+        <Card className='h-full'>
+          <CardContent className="flex items-center justify-center p-6">
+            <span className="text-4xl font-semibold">Werbung: {props.name}</span>
+          </CardContent>
+        </Card>
+      </Link>
+    </div>
   )
 }
 
